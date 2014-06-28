e8417ba877c99074cc10d61cb79d26406bb3ca2b
Vajrasky Kok <sky.kok@speaklikeaking.com>
2014-06-28 16:32:41 +0700

    Added interval setting.

--- src/wallpapoz_gui/wallpapoz_main_window.py
+++ src/wallpapoz_gui/wallpapoz_main_window.py
@@ -1,5 +1,7 @@
-from tkinter import Frame, TOP, LEFT, Y, YES, BOTH, NO, CENTER, X
+from tkinter import Frame, TOP, LEFT, Y, YES, BOTH, NO, CENTER, X, END, Label, Text, BOTTOM
 from tkinter.ttk import Treeview
+
+from lib.gettext import _
 
 tree = None
 
@@ -22,12 +24,38 @@ def _maketree(parent_tree, wallpapers, conf):
             tree.insert('', 'end', index, text=wallpaper_file)
     tree.pack(side=TOP, expand=YES, fill=BOTH)
 
+def _makesetting(setting_window, conf):
+    setting_label = Label(setting_window,
+                          text=_('Setting'),
+                          font=('courier', 20, 'bold'))
+    setting_label.pack()
+    interval_container = Frame(setting_window)
+    interval_container.pack(side=BOTTOM)
+    interval_label = Label(interval_container,
+                           text=_('Change wallpaper every '),
+                           font=('courier', 15, 'bold'))
+    interval_label.pack(side=LEFT)
+    interval_text = Text(interval_container, height=1, width=4)
+    interval_text.insert(END, conf['interval'])
+    interval_text.pack(side=LEFT)
+    minute_label = Label(interval_container,
+                         text=_(' minutes'),
+                         font=('courier', 15, 'bold'))
+    minute_label.pack(side=LEFT)
+
 def makemainwindow(parent, wallpapers, conf):
     main_window = Frame(parent)
-    tree_window = Frame(main_window)
-    image_window = Frame(main_window)
+    top_window = Frame(main_window)
+    tree_window = Frame(top_window)
+    image_window = Frame(top_window)
+    bottom_window = Frame(main_window)
+    setting_window = Frame(bottom_window)
 
     main_window.pack(side=TOP, expand=YES, fill=BOTH)
+    top_window.pack(side=TOP, expand=YES, fill=BOTH)
     tree_window.pack(side=TOP, expand=YES, fill=BOTH)
     image_window.pack(side=TOP, expand=YES, fill=BOTH)
+    bottom_window.pack(side=TOP, expand=YES, fill=BOTH)
+    setting_window.pack(side=TOP, expand=YES, fill=BOTH)
     _maketree(tree_window, wallpapers, conf)
+    _makesetting(setting_window, conf)
